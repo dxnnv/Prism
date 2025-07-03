cc890a7b948d6c506ca92eaaa90063fe34cb7a70
danny <danny@atropos.cc>
2025-07-03 12:48:15 -0400

    refactor!: separate main class operations into separate functions

--- src/main/kotlin/dev/dxnny/prism/Prism.kt
+++ src/main/kotlin/dev/dxnny/prism/Prism.kt
@@ -1,34 +1,38 @@
 package dev.dxnny.prism
 
 import dev.dxnny.infrastructure.Infrastructure
+import dev.dxnny.infrastructure.commands.CommandManager
 import dev.dxnny.infrastructure.files.Config
-import dev.dxnny.infrastructure.utils.ConsoleLog.logMessage
-import dev.dxnny.prism.commands.manager.CommandManager
+import dev.dxnny.infrastructure.files.Messages
+import dev.dxnny.infrastructure.utils.ConsoleLog
+import dev.dxnny.prism.commands.GradientCommand
+import dev.dxnny.prism.commands.PrismCommand
 import dev.dxnny.prism.events.PlayerJoinEvent
 import dev.dxnny.prism.events.PlayerQuitEvent
 import dev.dxnny.prism.files.Lang
-import dev.dxnny.prism.files.Lang.Companion.updateMessages
-import dev.dxnny.prism.files.Messages
 import dev.dxnny.prism.hooks.PlaceholderAPIHook
-import dev.dxnny.prism.storage.LiteManager
+import dev.dxnny.prism.manager.StorageManager
+import dev.dxnny.prism.manager.GradientManager
 import org.bukkit.command.ConsoleCommandSender
-import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.plugin.PluginManager
 import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.scheduler.BukkitTask
+
+private const val LOG_LIST_PREFIX = " <dark_gray>\\-<dark_gray><green>"
 
 class Prism : JavaPlugin() {
     companion object {
         lateinit var instance: Prism
+        lateinit var messages: Messages
+        lateinit var storage: StorageManager
     }
 
     lateinit var console: ConsoleCommandSender
     lateinit var configuration: Config
-    private lateinit var messages: Messages
-    private lateinit var storage: LiteManager
     lateinit var manager: PluginManager
     lateinit var infrastructure: Infrastructure
-    val version = "0.0.5"
+    val version: String = pluginMeta.version
+    val configVersion = "2.0.0"
+    val messagesVersion = "2.0.0"
 
     override fun onLoad() {
         this.manager = server.pluginManager
@@ -37,83 +41,74 @@ class Prism : JavaPlugin() {
     override fun onEnable() {
         instance = this
         infrastructure = Infrastructure(this)
-        infrastructure.setPluginLogPrefix("&8[&dPrism&8] ")
-        console = this.server.consoleSender
+        infrastructure.logPrefix = "<dark_gray>[<light_purple>Prism</light_purple>]<reset> "
+        console = server.consoleSender
+        sendStartupMessage()
 
-        // startup message
-        logMessage(" ")
-        logMessage("&c ______   &6______     &e__     &a______     &b__    __")
-        logMessage("&c/\\  == \\ &6/\\  == \\   &e/\\ \\   &a/\\  ___\\   &b/\\ \"-./  \\")
-        logMessage("&c\\ \\  _-/ &6\\ \\  __<   &e\\ \\ \\  &a\\ \\___  \\  &b\\ \\ \\-./\\ \\")
-        logMessage("&c \\ \\_\\   &6 \\ \\_\\ \\_\\ &e \\ \\_\\ &a \\/\\_____\\ &b \\ \\_\\ \\ \\_\\")
-        logMessage("&c  \\/_/   &6  \\/_/ /_/ &e  \\/_/ &a  \\/_____/ &b  \\/_/  \\/_/")
-        logMessage(" ")
-        // config
-        logMessage("&7Loading files...")
-        this.configuration = Config(this)
-        logMessage(" &8\\- &aLoaded config.yml")
+        setupConfig()
+        setupMessages()
+        setupStorage()
 
-        // messages
-        this.messages = Messages(this)
-        messages.setup()
-        updateMessages(this)
-        infrastructure.setPluginPrefix(Lang.prefix)
-        logMessage(" &8\\- &aLoaded messages.yml")
-
-        // Load data
-        val dbPath = this.dataFolder.path + "/prism.db"
-        this.storage = LiteManager(dbPath)
-        if (this.server.onlinePlayers.isNotEmpty()) {
-            storage.loadAllOnlinePlayersGradients(this.server.onlinePlayers)
-        }
-        logMessage(" &8\\- &aLoaded prism.db")
-
-        // commands
-        logMessage("&7Registering commands + events...")
-        CommandManager(this)
-        logMessage(" &8\\- &aRegistered commands")
-
-        // events
-        manager.registerEvents(PlayerJoinEvent(), this)
-        manager.registerEvents(PlayerQuitEvent(), this)
-        logMessage(" &8\\- &aRegistered events")
-
-        // PlaceholderAPI
-        logMessage("&7Registering PlaceholderAPI expansion...")
-        if (manager.isPluginEnabled("PlaceholderAPI")) {
-            PlaceholderAPIHook().register()
-        } else {
-            logger.warning("Unable to find PlaceholderAPI! Disabling plugin...")
-            this.manager.disablePlugin(this)
-        }
+        registerCommands()
+        registerListeners()
+        registerHooks()
     }
 
     override fun onDisable() {
-        try {
-            storage.saveAllPlayerGradients()
-        } catch (e: Exception) {
-            logger.severe("Error saving data: ${e.message}")
-            e.printStackTrace()
-        } finally {
-            storage.close()
+        storage.close()
+    }
+
+    private fun setupConfig() {
+        configuration = Config(this, configVersion)
+        GradientManager.loadGradientItems()
+    }
+
+    private fun setupMessages() {
+        messages = Messages(this, messagesVersion)
+        Lang.updateMessages()
+        infrastructure.prefix = Lang.prefix
+    }
+
+    private fun setupStorage() {
+        ConsoleLog.info("Initializing storage...")
+        storage = StorageManager("${dataFolder.path}/prism.db")
+        server.onlinePlayers.forEach {
+            storage.loadPlayer(it.uniqueId)
+        }
+        ConsoleLog.success("$LOG_LIST_PREFIX Loaded storage!")
+    }
+
+    private fun registerCommands() {
+        ConsoleLog.info("Registering commands...")
+        CommandManager(this, PrismCommand(this), GradientCommand())
+        ConsoleLog.success("$LOG_LIST_PREFIX Registered commands!")
+    }
+
+    private fun registerListeners() {
+        ConsoleLog.info("Registering listeners...")
+        manager.registerEvents(PlayerJoinEvent(), this)
+        manager.registerEvents(PlayerQuitEvent(), this)
+        ConsoleLog.success("$LOG_LIST_PREFIX Registered listeners!")
+    }
+
+    private fun registerHooks() {
+        ConsoleLog.info("Registering PlaceholderAPI expansion...")
+        if (manager.isPluginEnabled("PlaceholderAPI")) {
+            PlaceholderAPIHook().register()
+        } else {
+            ConsoleLog.fatal("Unable to find PlaceholderAPI! Disabling plugin...")
+            manager.disablePlugin(this)
         }
     }
 
-    @Suppress("unused")
-    fun runTaskAsynchronously(run: Runnable): BukkitTask {
-        return this.server.scheduler.runTaskAsynchronously(this, run)
-    }
-
-    // getters
-    fun getMessagesFile(): FileConfiguration {
-        return messages.get()!!
-    }
-
-    fun getMessages(): Messages {
-        return messages
-    }
-
-    fun getStorage(): LiteManager {
-        return storage
+    private fun sendStartupMessage() {
+        ConsoleLog.info(" ")
+        ConsoleLog.info("<rainbow> ______   ______     __     ______     __    __")
+        ConsoleLog.info("<rainbow>/\\  == \\ /\\  == \\   /\\ \\   /\\  ___\\   /\\ \"-./  \\")
+        ConsoleLog.info("<rainbow>\\ \\  _-/ \\ \\  __<   \\ \\ \\  \\ \\___  \\  \\ \\ \\-./\\ \\")
+        ConsoleLog.info("<rainbow> \\ \\_\\    \\ \\_\\ \\_\\  \\ \\_\\  \\/\\_____\\  \\ \\_\\ \\ \\_\\")
+        ConsoleLog.info("<rainbow>  \\/_/     \\/_/ /_/   \\/_/   \\/_____/   \\/_/  \\/_/")
+        ConsoleLog.info(" ")
+        ConsoleLog.info("<light_purple>Starting Prism v$version, created by dxnnv...")
     }
 }
